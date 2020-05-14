e97e9266bf558788068fe2947fcdf1a0241141e8
João Vinicius Rodrigues do Nascimento <joaoviniciusrodrigues@users.noreply.github.com>
2020-05-14 01:09:25 -0300

    Criação do componente header

--- CRUD/frontend/src/app/app.module.ts
+++ CRUD/frontend/src/app/app.module.ts
@@ -4,15 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/template/header/header.component';
+import {MatToolbarModule} from '@angular/material/toolbar'
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { NavComponent } from './components/template/nav/nav.component'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
