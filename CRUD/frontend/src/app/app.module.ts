0bfc8a38c435dd49be69f0fc68d35954c4b8d5f2
joaoviniciusrodrigues <joaoviniciusrodrigues@users.noreply.github.com>
2020-05-14 17:48:51 -0300

    joaoviniciusrodrigues

--- CRUD/frontend/src/app/app.module.ts
+++ CRUD/frontend/src/app/app.module.ts
@@ -5,24 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/template/header/header.component';
-import {MatToolbarModule} from '@angular/material/toolbar'
-import {MatSidenavModule} from '@angular/material/sidenav';
-import { NavComponent } from './components/template/nav/nav.component'
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { NavComponent } from './components/template/nav/nav.component';
+import { FooterComponent } from './components/template/footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavComponent
+    NavComponent,
+    FooterComponent,    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatSidenavModule
-    
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
